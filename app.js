488a3a60d49ba938c1dadb4935c7e34f3d28b993
Tejaswinibobba <tejaswinibobba@gmail.com>
2023-10-21 23:28:14 -0500

    new endpoint

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var mydataRouter = require('./routes/mydata');
 var app = express();
 
 // view engine setup
@@ -16,11 +16,13 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata', mydataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
